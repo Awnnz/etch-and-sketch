df08f4bfaa1764f9f6f8179ef4428a008da3eae7
Bryan <[200~130257764+Awnnz@users.noreply.github.com>
2023-05-22 21:49:41 -0700

    add 3 new functionality: color, rainbow, gradual

--- jss/index.js
+++ jss/index.js
@@ -1,27 +1,101 @@
-let row = 16;
-let column = 16;
+let size = 16;
+let mainContainer = document.querySelector('.main-container');
+let newBoardButton = document.querySelector('.newBoard');
+let colorButton = document.querySelector('.changeColor');
+let rainbowButton = document.querySelector('.rainbowColor')
+let gradualButton = document.querySelector('.gradual')
 
-let mainContainer = document.createElement('div');
-mainContainer.classList.add('main-container');
+newBoardButton.addEventListener('click', () => {
+  let value = +prompt('Size of Board?');
+  
+  while ((value < 2 || value > 100) || isNaN(value)) {
+    value = prompt('Size of Board?');
+  }
 
-document.body.appendChild(mainContainer);
+  clearBoard()
+  createboard(value)
+})
 
-for (let i = 0; i < row; i++) {
+colorButton.addEventListener('click', changeColor)
+rainbowButton.addEventListener('click', rainbowColor)
+gradualButton.addEventListener('click', rainbowGradualBlack)
+
+
+
+
+createboard(size)
+
+// FUNCTIONS GO HERE
+
+function createboard(size) {
+  
+  for (let i = 0; i < size; i++) {
     let newDiv = document.createElement('div');
     newDiv.classList.add('horizontal-container');
     mainContainer.appendChild(newDiv)
-}
+  }
 
-let horizontalContainers = document.querySelectorAll('.horizontal-container');
+  let horizontalContainers = document.querySelectorAll('.horizontal-container');
 
-horizontalContainers.forEach(container => {
-  for (let i = 0; i < column; i++) {
+  horizontalContainers.forEach(container => {
+  for (let i = 0; i < size; i++) {
     let newDiv = document.createElement('div');
       newDiv.classList.add('vertical');
       container.appendChild(newDiv)
   }
-})
+  })
 
+  let square = document.querySelectorAll('.vertical');
+  square.forEach(square => square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = "red";
+  }))
+}
 
-  
+function clearBoard() {
+    let squares = document.querySelectorAll('.horizontal-container', '.vertical');
+    
+    squares.forEach(square => square.remove())
+  }
+
+function changeColor() {
+  let square = document.querySelectorAll('.vertical');
+  let arr = [];
+  for (let i = 0; i < 3; i++) {
+    arr[i] = ~~(Math.random() * 255);
+  }
+  square.forEach(square => square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+  }))
+}
+
+function randomValue() {
+  return ~~(Math.random() * 255)
+}
+
+function rainbowColor() {
+  let square = document.querySelectorAll('.vertical');
+  square.forEach(square => square.addEventListener('mouseover', () => {
+    let arr = [];
+    for (let i = 0; i < 3; i++) {
+    arr[i] = ~~(Math.random() * 255);
+    }
+    square.style.backgroundColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+  }))
+}
+
+function rainbowGradualBlack() {
+  let darkBuffer = 1;
+  let square = document.querySelectorAll('.vertical');
+  square.forEach(square => square.addEventListener('mouseover', () => {
+    let arr = [];
+    for (let i = 0; i < 3; i++) {
+    arr[i] = ~~(Math.random() * 255);
+    }
+    square.style.backgroundColor = `rgb(${arr[0]*darkBuffer}, ${arr[1]*darkBuffer}, ${arr[2]*darkBuffer})`;
+    darkBuffer -= .10;
+  }))
+}
+
+// END OF FUNCTIONS
+
 
