6a2fe99fb5d8b8eaaa06026269ea076f8a3edf74
Bryan <[200~130257764+Awnnz@users.noreply.github.com>
2023-06-19 16:06:11 -0700

    fix initial click bug

--- jss/index.js
+++ jss/index.js
@@ -4,8 +4,21 @@ let newBoardButton = document.querySelector('.newBoard');
 let colorButton = document.querySelector('.changeColor');
 let rainbowButton = document.querySelector('.rainbowColor')
 let gradualButton = document.querySelector('.gradual')
-let click = true;
+let mouseDown = false
 
+document.body.onmousedown = (e) => {
+  e.preventDefault();
+  (mouseDown = true);
+  document.querySelector('.mode').textContent = `Drawing Mode: On`;
+}
+document.body.onmouseup = () => {
+  (mouseDown = false);
+  document.querySelector('.mode').textContent = `Drawing Mode: Off`;
+}
+
+colorButton.addEventListener('click', randomColor)
+rainbowButton.addEventListener('click', rainbowColor)
+gradualButton.addEventListener('click', rainbowGradualBlack)
 newBoardButton.addEventListener('click', () => {
   let value = +prompt('Size of Board?');
   
@@ -16,13 +29,6 @@ newBoardButton.addEventListener('click', () => {
   clearBoard()
   createboard(value)
 })
-
-colorButton.addEventListener('click', randomColor)
-rainbowButton.addEventListener('click', rainbowColor)
-gradualButton.addEventListener('click', rainbowGradualBlack)
-
-
-
 
 createboard(size)
 
@@ -48,14 +54,19 @@ function createboard(size) {
 
   let square = document.querySelectorAll('.vertical');
   square.forEach(square => square.addEventListener('mouseover', () => {
-    if (click) square.style.backgroundColor = "red";
+    if (mouseDown) square.style.backgroundColor = "black";
     
   }))
+
+  square.forEach(square => square.addEventListener('mousedown', () => {
+     square.style.backgroundColor = "black";
+  }))
+
+
 }
 
 function clearBoard() {
     let squares = document.querySelectorAll('.horizontal-container', '.vertical');
-    
     squares.forEach(square => square.remove())
   }
 
@@ -66,7 +77,11 @@ function randomColor() {
     arr[i] = ~~(Math.random() * 255);
   }
   square.forEach(square => square.addEventListener('mouseover', () => {
-    if (click) square.style.backgroundColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+    if (mouseDown) square.style.backgroundColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+  }))
+
+  square.forEach(square => square.addEventListener('mousedown', () => {
+    square.style.backgroundColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
   }))
 }
 
@@ -81,7 +96,15 @@ function rainbowColor() {
     for (let i = 0; i < 3; i++) {
     arr[i] = ~~(Math.random() * 255);
     }
-    if (click) square.style.backgroundColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+    if (mouseDown) square.style.backgroundColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+  }))
+
+  square.forEach(square => square.addEventListener('mousedown', () => {
+    let arr = [];
+    for (let i = 0; i < 3; i++) {
+    arr[i] = ~~(Math.random() * 255);
+    }
+    square.style.backgroundColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
   }))
 }
 
@@ -89,20 +112,27 @@ function rainbowGradualBlack() {
   let darkBuffer = 1;
   let square = document.querySelectorAll('.vertical');
   square.forEach(square => square.addEventListener('mouseover', () => {
+    if (!mouseDown) return;
     let arr = [];
     for (let i = 0; i < 3; i++) {
     arr[i] = ~~(Math.random() * 255);
     }
-    if (click) square.style.backgroundColor = `rgb(${arr[0]*darkBuffer}, ${arr[1]*darkBuffer}, ${arr[2]*darkBuffer})`;
-    darkBuffer -= .10;
+    if (mouseDown) square.style.backgroundColor = `rgb(${arr[0]*darkBuffer}, ${arr[1]*darkBuffer}, ${arr[2]*darkBuffer})`;
+    darkBuffer -= .01;
+  }))
+
+  square.forEach(square => square.addEventListener('mousedown', () => {
+    if (!mouseDown) return;
+    let arr = [];
+    for (let i = 0; i < 3; i++) {
+    arr[i] = ~~(Math.random() * 255);
+    }
+    square.style.backgroundColor = `rgb(${arr[0]*darkBuffer}, ${arr[1]*darkBuffer}, ${arr[2]*darkBuffer})`;
+    darkBuffer -= .01;
   }))
 }
 
-document.querySelector('.main-container').addEventListener('click', () => {
-  let mode = document.querySelector('.mode');
-  click = !click;
-  click ? mode.textContent = `Drawing Mode: On` :  mode.textContent = `Drawing Mode: Off`;
-})
+
 
 // END OF FUNCTIONS
 
